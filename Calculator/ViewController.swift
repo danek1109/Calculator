d3ed386a73aeb36810b653eff8820e0fb14cb75e
даниил <rex1109@mail.ru>
2021-11-13 02:34:48 +0300

    first

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var buton: UIButton!
+    @IBOutlet weak var textfield1: UITextField!
+    @IBOutlet weak var textfield2: UITextField!
+    
+    var const1 = 0
+    var const2 = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
     }
 
-
+   
+    
+    @IBAction func buttonAct(_ sender: UIButton) {
+        print(const1)
+    }
+    
+    
+    @IBAction func textfield1(_ sender: UITextField) {
+        const1 = Int(textfield1.text ?? "") ?? 0
+    }
+    
+    
+    @IBAction func textfield2(_ sender: UITextField) {
+        
+    }
 }
+
 
